192a2b8f35c8e2c589f9520ba75d50d22fc86e39
Nojoponken <71374879+Nojoponken@users.noreply.github.com>
2021-04-09 17:40:22 +0200

    local storage maybe??

--- app.js
+++ app.js
@@ -1,43 +1,16 @@
-class flashGame {
-  constructor(Name, Description) {
-    this.name = Name;
-    this.description = Description;
-    this.ratings = [5, 7, 4, 8, 8, 7, 9, 10, 8, 10];
-    this.averageRating = 0.0;
-  }
-  generateGameCard() {
-    let card = document.createElement("div");
-    card.setAttribute("id", "cardBody");
+"use strict";
+const fg = require("./fg.js");
+let flashGame = fg.flashGame;
 
-    let title = document.createElement("h2");
-    title.setAttribute("id", "cardTitle");
-    title.innerHTML = this.name;
-    let text = document.createElement("p");
-    text.setAttribute("id", "cardText");
-    text.innerHTML = this.description;
-    let score = document.createElement("p");
-    score.setAttribute("id", "cardScore");
-    score.innerHTML = this.averageRating;
+const LSKG = "app.games";
 
-    card.appendChild(title);
-    card.appendChild(text);
-    card.appendChild(score);
+let game = JSON.parse(localStorage.getItem(LSKG)) || [];
 
-    document.getElementById("cardDiv").appendChild(card);
-  }
+game.forEach((flashGame) => {
+  flashGame.updateAverageRating();
+  flashGame.generateGameCard();
+});
 
-  updateAverageRating() {
-    this.averageRating = 0;
-    this.ratings.forEach((int) => {
-      this.averageRating += int;
-    });
-    this.averageRating =
-      Math.round(
-        (this.averageRating / this.ratings.length + Number.EPSILON) * 10
-      ) / 10;
-  }
-}
-let game = [];
 let gameForm = document.querySelector("#gameForm");
 let gfTitle = document.querySelector("#title");
 let gfDesc = document.querySelector("#description");
@@ -51,12 +24,27 @@ gameForm.addEventListener("submit", (e) => {
   ) {
     return;
   }
-  game.push(new flashGame(gfTitle.value.trim(), gfDesc.value.trim()));
+  game.push(
+    new flashGame(
+      gfTitle.value.trim(),
+      gfDesc.value.trim(),
+      gfRating.value.trim()
+    )
+  );
   gfTitle.value = "";
   gfDesc.value = "";
+  gfRating.value = "";
+  updateGames();
 });
 
-game.forEach((flashGame) => {
-  flashGame.updateAverageRating();
-  flashGame.generateGameCard();
-});
+function updateGames() {
+  save();
+  game.forEach((flashGame) => {
+    flashGame.updateAverageRating();
+    flashGame.generateGameCard();
+  });
+}
+
+function save() {
+  localStorage.setItem(LSKG, JSON.stringify(game));
+}
